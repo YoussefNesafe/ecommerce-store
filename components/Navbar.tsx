672cc414e7a1259c405d0e7db2e6962f39c20a9e
YoussefNesafe <ynessafe@gmail.com>
2023-09-15 14:03:40 +0400

    fix: navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,7 +9,7 @@ export const revalidate = 0;
 const Navbar = async () => {
   const categories = await getCategories()
   return (
-    <div className="border-b">
+    <div className="border-b sticky top-0 left-0 z-[2] bg-white">
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
           <Link href='/' className="ml-4 flex lg:ml-0 gap-x-2">
